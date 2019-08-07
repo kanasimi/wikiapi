0b5b4b4df1dee2664eef857dfc76264b6552d2ab
kanasimi <kanasimi@gmail.com>
2019-08-07 22:49:10 +0800

    add function add_tests(test_name, conditions)

--- _test suite/test.js
+++ _test suite/test.js
@@ -13,21 +13,39 @@ CeL.run('application.debug.log');
 
 /** {ℕ⁰:Natural+0}count of all errors (failed + fatal) */
 let all_error_count = 0;
+/** {ℕ⁰:Natural+0}all tests count */
+let all_tests = 0;
 /** {ℕ⁰:Natural+0}tests still running */
 let still_running = 0;
 
+function add_tests(test_name, conditions) {
+	if (!conditions) {
+		// shift arguments: 跳過 test_name。
+		conditions = test_name;
+		test_name = null;
+	}
+
+	all_tests++;
+	still_running++;
+	CeL.test(test_name, conditions, check_tests);
+}
+
 function check_tests(recorder, error_count) {
 	all_error_count += error_count;
-	if (--still_running > 0 || all_error_count === 0)
+	if (--still_running > 0) {
 		return;
+	}
+
+	if (all_error_count === 0) {
+		CeL.info('All tests OK.');
+	}
 
 	throw new Error('All %error@1.', all_error_count);
 }
 
 // ============================================================================
 
-still_running++;
-CeL.test('load page', async (assert, setup_test, finish_test) => {
+add_tests('load page', async (assert, setup_test, finish_test) => {
 	setup_test('load page: [[w:en:Universe]]');
 	let wiki = new wikiapi;
 	let page = await wiki.page('Universe');
@@ -36,12 +54,11 @@ CeL.test('load page', async (assert, setup_test, finish_test) => {
 	assert(page.wikitext.includes('space]]')
 		&& page.wikitext.includes('time]]'), 'wikitext');
 	finish_test('load page: [[w:en:Universe]]');
-}, check_tests);
+});
 
 // ------------------------------------------------------------------
 
-still_running++;
-CeL.test('edit page', async (assert, setup_test, finish_test) => {
+add_tests('edit page', async (assert, setup_test, finish_test) => {
 	setup_test('edit page');
 	const test_page_title = 'Project:Sandbox';
 	const test_wikitext = '\nTest edit using {{GitHub|kanasimi/wikiapi}}.';
@@ -68,12 +85,11 @@ CeL.test('edit page', async (assert, setup_test, finish_test) => {
 
 	// console.log('Done.');
 	finish_test('edit page');
-}, check_tests);
+});
 
 // ------------------------------------------------------------------
 
-still_running++;
-CeL.test('parse page en', async (assert, setup_test, finish_test) => {
+add_tests('parse page en', async (assert, setup_test, finish_test) => {
 	setup_test('parse page en');
 	let user_name = null;
 	let password = null;
@@ -86,12 +102,11 @@ CeL.test('parse page en', async (assert, setup_test, finish_test) => {
 		(token) => template_list.push(token.name));
 	assert(template_list.includes('Infobox'), '[[w:en:Universe]] must includes {{Infobox}}');
 	finish_test('parse page en');
-}, check_tests);
+});
 
 // ------------------------------------------------------------------
 
-still_running++;
-CeL.test('parse page zh', async (assert, setup_test, finish_test) => {
+add_tests('parse page zh', async (assert, setup_test, finish_test) => {
 	setup_test('parse page zh');
 	let zhwiki = new wikiapi('zh');
 	let page = await zhwiki.page('宇宙');
@@ -100,24 +115,22 @@ CeL.test('parse page zh', async (assert, setup_test, finish_test) => {
 		(token) => template_list.push(token.name));
 	assert(template_list.includes('Infobox'), '[[w:zh:宇宙]] must includes {{Infobox}}');
 	finish_test('parse page zh');
-}, check_tests);
+});
 
 // ------------------------------------------------------------------
 
-still_running++;
-CeL.test('read wikidata', async (assert, setup_test, finish_test) => {
+add_tests('read wikidata', async (assert, setup_test, finish_test) => {
 	setup_test('read wikidata');
 	let wiki = new wikiapi;
 	// Q1: Universe
 	let page = await wiki.data('Q1');
 	assert([CeL.wiki.data.value_of(page.labels.zh), '宇宙'], 'zh label of Q1 is 宇宙');
 	finish_test('read wikidata');
-}, check_tests);
+});
 
 // ------------------------------------------------------------------
 
-still_running++;
-CeL.test('read wikidata #2', async (assert, setup_test, finish_test) => {
+add_tests('read wikidata #2', async (assert, setup_test, finish_test) => {
 	setup_test('read wikidata #2');
 	let wiki = new wikiapi;
 	// P1419: shape
@@ -126,16 +139,15 @@ CeL.test('read wikidata #2', async (assert, setup_test, finish_test) => {
 	// console.log(data);
 	assert(data.includes('shape of the universe'), '`shape` of the `Universe` is Q1647152 (shape of the universe)');
 	finish_test('read wikidata #2');
-}, check_tests);
+});
 
 // ------------------------------------------------------------------
 
-still_running++;
-CeL.test('get list of category', async (assert, setup_test, finish_test) => {
+add_tests('get list of category', async (assert, setup_test, finish_test) => {
 	setup_test('get list of [[w:en:Category:Chemical_elements]]');
 	let wiki = new wikiapi;
 	let list = await wiki.categorymembers('Chemical elements');
 	assert(list.map((page_data) => page_data.title).includes('Iron'), 'Iron is a chemical element');
 	finish_test('get list of [[w:en:Category:Chemical_elements]]');
-}, check_tests);
+});
 
