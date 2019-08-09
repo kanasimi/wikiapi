1c1a41e02b71ebde5104355b36d72d55d110e33c
kanasimi <kanasimi@gmail.com>
2019-08-09 15:21:08 +0800

    add application.locale

--- _test suite/test.js
+++ _test suite/test.js
@@ -9,7 +9,9 @@ CeL.info('Using CeJS version: ' + CeL.version);
 // load modules for test
 CeL.run(['application.debug.log',
 	// gettext(), and for .detect_HTML_language(), .time_zone_of_language()
-	'application.locale.gettext'
+	'application.locale.gettext',
+	// for localized messages
+	'application.locale'
 ]);
 
 // ============================================================================
@@ -197,6 +199,8 @@ add_test('read wikidata', async (assert, setup_test, finish_test) => {
 	const page = await wiki.data('Q1', {
 		props: 'labels'
 	});
+	CeL.info('page:');
+	console.log(page);
 	assert([CeL.wiki.data.value_of(page.labels.zh), '宇宙'], 'zh label of Q1 is 宇宙');
 	finish_test('read wikidata');
 });
